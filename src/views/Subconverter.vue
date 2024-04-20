36d986c794418b6ba31a7842997724e9d2660faf
xingan210 <74850415+xingan210@users.noreply.github.com>
2024-04-20 23:42:10 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -474,9 +474,9 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
+          "自用": "http://xn--0trs2na9na7ynvh70o.love:25500",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://apiurl.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
           nameless13提供: "https://www.nameless13.com",
           subconverter作者提供: "https://sub.xeton.dev",
           "sub-web作者提供": "https://api.wcc.best",
